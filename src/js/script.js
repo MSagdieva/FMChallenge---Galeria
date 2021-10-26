8eccfab5ff0de868d9f25e297b9755f4c4c59f0b
MSagdieva <grigorieva.ms@yandex.ru>
2021-10-26 09:51:20 +0300

    adding json names

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,12 @@
-let names=[];
+// fetching info for description
+let PicsNames=document.querySelectorAll(".painting__name");
+let PicsAuthor=document.querySelectorAll(".painting__author");
 fetch('data/data.json')
     .then(response => response.json())
-    .then(pics=>{pics.forEach(element => {
-        names.push(element.name);
-    });});
-console.log(names);
-let i = 0;
-document.querySelectorAll(".painting__name").forEach(element => {
-        element.insertAdjacentText("beforeend", names[4]);
-    });
+    .then(pics=>{
+        for(let i=0;i<pics.length;i++){
+        PicsNames[i].insertAdjacentText("beforeend", pics[i].name);
+        PicsAuthor[i].insertAdjacentText("beforeend", pics[i].artist.name);
+    }
+})       
+    .catch(error=>{console.log(error.data)});
