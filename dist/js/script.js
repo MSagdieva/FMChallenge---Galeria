aa0dd1d0af4a3f5d98d01e00cb794479fef25458
MSagdieva <grigorieva.ms@yandex.ru>
2021-10-26 06:59:15 +0300

    starting with json

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,30 +1,11 @@
-let form = document.querySelector(".subscribe-form");
-let email = document.querySelector(".email");
-let mes = document.querySelector(".error-message");
-
-
-function validateForm(){
-form.addEventListener('submit',function(e){
-    e.preventDefault();
-    if (!email.value){
-        mes.innerHTML = "Whoops! It looks like you forgot to add your email"
-        email.classList.add('error');
-    }
-    else
-    {if(!(validateEmail(email.value))){
-        mes.innerHTML = "Please provide a valid email"
-        email.classList.add('error');
-    }
-    else{
-        email.classList.remove('error');
-    }}
-})
-}
-
-
-function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-validateForm();
-console.log(validateEmail("grigorieva.ms@yandex.ru"));
+let names=[];
+fetch('data/data.json')
+    .then(response => response.json())
+    .then(pics=>{pics.forEach(element => {
+        names.push(element.name);
+    });});
+console.log(names);
+let i = 0;
+document.querySelectorAll(".painting__name").forEach(element => {
+        element.insertAdjacentText("beforeend", names[4]);
+    });
